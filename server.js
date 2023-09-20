a6b313b2a6aeb4ec74b8621465bba46398c7f4f9
RDCoder8 <mojinr@gmail.com>
2023-09-20 10:26:49 -0400

    Routes have been refractored into their own file.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const Logs = require('./models/logs')
 const seedLogs = require('./seed/seedLogs')
+const logRoutes = require('./controllers/logs')
 
 ////////Database Connection
 mongoose.connect(process.env.MONGO_URI, {
@@ -26,90 +27,7 @@ app.engine("jsx", jsxViewEngine())
 app.use(express.urlencoded({extended: false}))
 app.use(methodOverride('_method'));
 
-//Seed route
-app.get('/logs/seed', async (req, res) => {
-    try {
-        const createdLog = await Logs.create(seedLogs)
-        res.status(201).send(createdLog)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-    
-})
-
-//Index
-app.get("/logs", async (req, res) => {
-    try {
-        const foundLogs = await Logs.find({})
-        res.render("Index", {logs: foundLogs})
-    } catch (error) {
-        res.status(400).send(error)
-    }
-    
-})
-
-//New
-app.get('/logs/new', (req, res) => {
-    res.render('New')
-})
-
-//Delete
-app.delete('/logs/:id', async (req, res) => {
-    try {
-        await Logs.findByIdAndDelete(req.params.id)
-        res.status(200).redirect("/logs")
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
-
-//Update
-app.put('/logs/:id', async (req, res) => {
-    try {
-        req.body.shipIsBroken = req.body.shipIsBroken === "on"
-        const updatedLog = await Logs.findByIdAndUpdate(req.params.id, req.body, {new: true})
-        res.status(201).redirect("/logs")
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
-
-//Create
-app.post('/logs', async (req, res) => {
-    try {
-        req.body.shipIsBroken = req.body.shipIsBroken === "on"
-        const createdLog = await Logs.create(req.body)
-        res.status(201).redirect("/logs")
-    } catch (error) {
-        res.status(400).send(error)
-    }
-    
-})
-
-//Edit
-app.get('/logs/:id/edit', async (req, res) => {
-    try {
-        const foundLog = await Logs.findById(req.params.id)
-        res.status(200).render('Edit', {log : foundLog})
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
-
-//Show
-app.get('/logs/:id', async (req, res) => {
-    try {
-        const foundLog = await Logs.findById(req.params.id)
-        res.status(200).render("Show", {log : foundLog})
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
-
-//Catch all
-app.get('*', (req, res) => {
-    res.redirect('/logs')
-})
+app.use("/", logRoutes)
 
 app.listen(PORT, () => {
     console.log(`Incoming messaging on port ${PORT} captain!`)
